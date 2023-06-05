5cf5232ff5e9f68e396880ed5a4d528f83078669
Yongaci, Asil Poyraz <poyrazyongaci507@gmail.com>
2023-06-05 10:20:18 +0300

    FFT_Plot Initial Algorithm
    functionaly working algorithm for FFT plot

---
+++ DFT.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def calculate_dft(sequence):
+    N = len(sequence)
+    dft_result = []
+    
+    for k in range(N):
+        dft_value = 0
+        for n in range(N):
+            exponent = -2j * np.pi * k * n / N
+            dft_value += sequence[n] * np.exp(exponent)
+        dft_result.append(dft_value)
+    
+    return dft_result
+
+# Example usage
+sequence = [1, 2, 3, 4, 5]
+
+dft_result = calculate_dft(sequence)
+print("DFT Result:")
+for index, value in enumerate(dft_result):
+    print(f"DFT[{index}]: {value}")
